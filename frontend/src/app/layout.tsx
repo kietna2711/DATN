b67c336a96947511c74989c52d2bac84d4d12228
kietna2711 <kietanh27112005@gmail.com>
2025-05-21 10:48:37 +0700

    build app

---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,14 @@
+import "@/app/globals.css";
+import Footer from "./components/Footer";
+
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="vi">
+      <body>
+        <main>{children}</main>
+        <Footer/>
+      </body>
+    </html>
+  );
+}
---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,9 @@
+import Banner from "./components/Banner";
+
+export default function HomePage() {
+  return (
+    <main>
+      <Banner/> 
+    </main>
+  );
+}
