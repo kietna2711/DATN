f81c2d0bb0ff2cf493cc0d64d89dcf4613d30960
xuan1808 <xn43685@gmail.com>
2025-06-01 14:05:19 +0700

    tttttttt

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -80,6 +80,8 @@ const getProductById = async (req, res) => {
   try {
     const product = await products
       .findById(req.params.id)
+      .populate('categoryId')        
+      .populate('subcategoryId')    
       .populate('variants');
     res.json(product);
   } catch (error) {
