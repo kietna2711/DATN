d0cf2919c9606fbd053abe02708ab10d0cb45e20
tuongdt <dinhvantuong2192005@gmail.com>
2025-06-02 08:44:26 +0700

    up

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/Shopgaubong')
+mongoose.connect('mongodb://localhost:27017/Shopgaubong')
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.log(err));
 
