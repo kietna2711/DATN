de7c918bf882db83ec6aa9888156faeba483911a
kietna2711 <kietanh27112005@gmail.com>
2025-06-04 10:03:43 +0700

    hmm

--- frontend/src/app/types/productD.tsx
+++ frontend/src/app/types/productD.tsx
@@ -3,12 +3,11 @@ import { Variant } from "./variantD";
 
 
 export interface Products {
-  image: any;
   _id: string;           // id dạng string theo JSON bạn nhận
   name: string;
   description: string;
   price: number;
-  images: string[];                    // Mảng đường dẫn ảnh
+  images: [];                    // Mảng đường dẫn ảnh
   categoryId: Category;               // Sau khi populate: là 1 object
   createdAt: Date;
   variants: Variant[];               // Mảng biến thể đã populate
