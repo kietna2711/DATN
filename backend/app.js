7c35d46e62d34bd77dc908e15a326f9a23e898ea
baonopro05 <quocbaotran2k5@gmail.com>
2025-06-11 14:51:51 +0700

    quocbao
    thanh toán

--- backend/app.js
+++ backend/app.js
@@ -21,19 +21,13 @@ var categoriesRouter = require('./routes/categories');
 var productsRouter = require('./routes/products');
 const variantsRouter = require('./routes/variants');
 const subcategoryRouter = require('./routes/subcategory');
+const paymentRouter = require("./routes/payment"); //thanh toán
+const orderRoutes = require("./routes/order"); //đơn hàng
 
 const authenticateToken = require('./middleware/auth');
 const reviewRoutes = require('./routes/review');
-
 const usersProfileRoutes = require('./routes/userprofile'); // Đường dẫn đến routes usersProfile
-
 const favoriteRouter = require('./routes/favorites');
-const orderRoutes = require("./routes/order"); //đơn hàng
-
-const paymentRouter = require("./routes/payment"); //thanh toán
-const paymentReturnRouter = require("./routes/paymentReturn");
-
-
 
 var app = express();
 
@@ -73,8 +67,6 @@ async (_accessToken, _refreshToken, profile, done) => {
 }
 ));
 
-
-
 passport.serializeUser((user, done) => {
   done(null, user.id);
 });
@@ -91,31 +83,25 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
-
 app.use(express.static(path.join(__dirname, 'public')));
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/categories', categoriesRouter);
 app.use('/products', productsRouter);
 app.use('/variants', variantsRouter);
 app.use('/subcategory', subcategoryRouter);
-
-// không cần token khi thanh toán momo
-app.use("/payment", paymentRouter);
-app.use("/", paymentReturnRouter); // Để route /payment-return hoạt động tại http://localhost:3000/payment-return
-// 
-app.use(authenticateToken); // Bảo vệ các route sau khi xác thực token
-app.use('/favorites', favoriteRouter);
-// thanh toán
 app.use("/reviews", reviewRoutes);
-app.use("/reviews", require("./routes/review"));
 app.use('/api/usersProfile', usersProfileRoutes);
+app.use("/payment", paymentRouter); //Momo, thanh toán
+app.use(require('./routes/payment')); //IPN
+// app.use("/payment", require("./routes/payment")); //đường dẫn file routes/payment.js
 app.use("/orders", orderRoutes);
 
-
-
+app.use(authenticateToken); // Bảo vệ các route sau khi xác thực token
+app.use('/favorites', favoriteRouter);
+app.use("/reviews", require("./routes/review"));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -127,7 +113,6 @@ app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
   // render the error page
   res.status(err.status || 500);
   res.render('error');
